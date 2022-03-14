65ed0c614c9f1a47647b1491e224cfd517cdfd4e
ritik ramuka <ritik.ramuka@gmail.com>
2022-03-15 00:22:30 +0530

    14-03-2022 FJP-2

--- Binary Tree/Construction/Main.java
+++ Binary Tree/Construction/Main.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
-public class Main {
-    public static class TreeNode {
+class Main {
+    // Binary Tree Class
+    public static class Node {
         int val;
-        TreeNode left;
-        TreeNode right;
+        Node left;
+        Node right;
 
-        TreeNode() {
+        Node() {
         }
 
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
+        Node(int val, Node left, Node right) {
             this.left = left;
             this.right = right;
+            this.val = val;
         }
     }
 
     public static class Pair {
-        TreeNode node;
+        Node node;
         int state;
 
         Pair() {
         }
 
-        Pair(TreeNode node, int state) {
-            this.node = node;
+        Pair(Node node, int state) {
             this.state = state;
+            this.node = node;
         }
     }
 
-    public static TreeNode Construct(Integer[] arr) {
+    public static Node Construct(Integer[] arr) {
         if (arr.length == 0) {
-            return new TreeNode();
+            return new Node();
         }
 
-        TreeNode root = new TreeNode(arr[0], null, null);
+        Node root = new Node(arr[0], null, null);
         Pair rtp = new Pair(root, 1);
 
         Stack<Pair> st = new Stack<>();
@@ -43,8 +44,9 @@ public class Main {
         while (i < arr.length) {
             Pair top = st.peek();
             if (top.state == 1) {
+                // left
                 if (arr[i] != null) {
-                    TreeNode left = new TreeNode(arr[i], null, null);
+                    Node left = new Node(arr[i], null, null);
                     top.node.left = left;
 
                     Pair lp = new Pair(left, 1);
@@ -53,8 +55,9 @@ public class Main {
                 i++;
                 top.state++;
             } else if (top.state == 2) {
+                // right
                 if (arr[i] != null) {
-                    TreeNode right = new TreeNode(arr[i], null, null);
+                    Node right = new Node(arr[i], null, null);
                     top.node.right = right;
 
                     Pair rp = new Pair(right, 1);
@@ -70,7 +73,7 @@ public class Main {
         return root;
     }
 
-    public static void display(TreeNode root) {
+    public static void display(Node root) {
         if (root == null)
             return;
 
@@ -94,168 +97,74 @@ public class Main {
         display(root.right);
     }
 
-    public static int size(TreeNode node) {
-        if (node == null) {
+    public static int size(Node root) {
+        if(root == null) {
             return 0;
         }
-        return size(node.left) + size(node.right) + 1;
+        return size(root.left) + size(root.right) + 1;
     }
 
-    public static int sum(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-        return sum(node.left) + sum(node.right) + node.val;
+    public static int sum(Node root) {
+        if(root == null) return 0;
+        return sum(root.left) + root.val + sum(root.right);
     }
 
-    public static int max(TreeNode node) {
-        if (node == null) {
-            return Integer.MIN_VALUE;
-        }
-        return Math.max(max(node.left), Math.max(max(node.right), node.val));
+    public static int max(Node root) {
+        if(root == null) return Integer.MIN_VALUE;
+        return Math.max(max(root.left), Math.max(max(root.right), root.val));
     }
 
-    public static int height(TreeNode node) {
-        if (node == null) {
-            return -1;
-        }
-        return Math.max(height(node.left), height(node.right)) + 1;
+    public static int height(Node root) {
+        if(root == null) return -1;
+        return Math.max(height(root.left), height(root.right)) + 1;
     }
 
-    public static void preorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+    public static void preorder(Node root) {
+        if(root == null) return;
+        // pre
         System.out.print(root.val + " ");
         preorder(root.left);
+        // in
         preorder(root.right);
+        // post
     }
 
-    public static void inorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+    public static void inorder(Node root) {
+        if(root == null) return;
+        // pre
         inorder(root.left);
+        // in
         System.out.print(root.val + " ");
         inorder(root.right);
+        // post
     }
 
-    public static void postorder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+    public static void postorder(Node root) {
+        if(root == null) return;
+        // pre
         postorder(root.left);
+        // in
         postorder(root.right);
+        // post
         System.out.print(root.val + " ");
-    }
-
-    public static void levelOrder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                TreeNode node = q.poll();
-                System.out.print(node.val + " ");
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void iterativePreTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while (!st.isEmpty()) {
-            TreeNode node = st.pop();
-            System.out.print(node.val + " ");
-            if (node.right != null) {
-                st.push(node.right);
-            }
-            if (node.left != null) {
-                st.push(node.left);
-            }
-        }
-    }
-
-    public static void iterativeInTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode node = root;
-        while (node != null || !st.isEmpty()) {
-            while (node != null) {
-                st.push(node);
-                node = node.left;
-            }
-            node = st.pop();
-            System.out.print(node.val + " ");
-            node = node.right;
-        }
-    }
-
-    public static void iterativePostTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode node = root;
-        TreeNode prev = null;
-        while (node != null || !st.isEmpty()) {
-            while (node != null) {
-                st.push(node);
-                node = node.left;
-            }
-            node = st.peek();
-            if (node.right == null || node.right == prev) {
-                System.out.print(node.val + " ");
-                prev = st.pop();
-                node = null;
-            } else {
-                node = node.right;
-            }
-        }
     }
 
     public static void main(String[] args) {
-        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
-                null };
-        TreeNode root = Construct(arr);
+        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 57, null, null };
+        Node root = Construct(arr);
         // display(root);
-        System.out.println("Size: " + size(root));
-        System.out.println("Sum: " + sum(root));
-        System.out.println("Max: " + max(root));
-        System.out.println("Height: " + height(root));
-        System.out.println("Preorder: ");
+        System.out.println("Size :" + size(root));
+        System.out.println("Sum : " + sum(root));
+        System.out.println("Height : " + height(root));
+        System.out.println("Max : " + max(root));
+        System.out.println("Preorder : ");
         preorder(root);
         System.out.println();
-        System.out.println("Inorder: ");
+        System.out.println("Inorder : ");
         inorder(root);
         System.out.println();
-        System.out.println("Postorder: ");
+        System.out.println("Postorder : ");
         postorder(root);
         System.out.println();
-        System.out.println("LevelOrder: ");
-        levelOrder(root);
-        System.out.println("IterativePreTraversal: ");
-        iterativePreTraversal(root);
-        System.out.println();
-        System.out.println("IterativeInTraversal: ");
-        iterativeInTraversal(root);
-        System.out.println();
-        System.out.println("IterativePostTraversal: ");
-        iterativePostTraversal(root);
     }
 }
